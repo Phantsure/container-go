c3f4398780facb1f71ea27497700adb8b4383f37
phantsure <phantsure@pop-os.localdomain>
2020-09-16 00:53:28 +0530

    made a container using golang

---
+++ README.md
@@ -0,0 +1,1 @@
+# Containers along Golang
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
+
+// docker 		  run image <cmd> <params>
+// go run main.go run		<cmd> <params>
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("help")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	// Control Group for memory and procs limit
+	cg()
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/ubuntu-fs/")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+
+	syscall.Unmount("/proc", 0)
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	err := os.Mkdir(filepath.Join(pids, "phan"), 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	must(ioutil.WriteFile(filepath.Join(pids, "phan/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(ioutil.WriteFile(filepath.Join(pids, "phan/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "phan/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
